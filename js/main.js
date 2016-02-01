e63523d6ebcfe1e337e8acce077049ccd41bbd92
Kowkin <kowkin89@gmail.com>
2016-02-01 05:34:18 +0200

    mobile first

--- js/main.js
+++ js/main.js
@@ -23,57 +23,67 @@
 //});
 
 //animated-MENU
-jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MQL = 1170;
+jQuery(document).ready(function ($) {
+    //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+    var MQL = 1170;
 
-	//primary navigation slide-in effect
-	if($(window).width() > MQL) {
-		var headerHeight = $('.cd-header').height();
-		$(window).on('scroll',
-		{
-	        previousTop: 0
-	    });
-	}
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.cd-header').height();
+        $(window).on('scroll', {
+            previousTop: 0
+        });
+    }
 
-	//open/close primary navigation
-	$('.cd-primary-nav-trigger').on('click', function(){
-		$('.cd-menu-icon').toggleClass('is-clicked'); 
-		$('.cd-header').toggleClass('menu-is-open');
-		
-		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-		if( $('.cd-primary-nav').hasClass('is-visible') ) {
-			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
-				$('body').removeClass('overflow-hidden');
-			});
-		} else {
-			$('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
-				$('body').addClass('overflow-hidden');
-			});	
-		}
-	});
+    //open/close primary navigation
+    $('.cd-primary-nav-trigger').on('click', function () {
+        $('.cd-menu-icon').toggleClass('is-clicked');
+        $('.cd-header').toggleClass('menu-is-open');
+
+        //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        if ($('.cd-primary-nav').hasClass('is-visible')) {
+            $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').removeClass('overflow-hidden');
+            });
+        } else {
+            $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').addClass('overflow-hidden');
+            });
+        }
+    });
+    $(".cd-primary-nav  a").click(function () {
+        $('.cd-menu-icon').toggleClass('is-clicked');
+        $('.cd-header').toggleClass('menu-is-open');
+
+        //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        if ($('.cd-primary-nav').hasClass('is-visible')) {
+            $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').removeClass('overflow-hidden');
+            });
+        } else {
+            $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').addClass('overflow-hidden');
+            });
+        }
+    });
 });
 
+
 // Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+$(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 //mail_to
 //setup email here
-$(function(){
-   $('#button-send').click(function (event) {
+$(function () {
+    $('#button-send').click(function (event) {
         $('#button-send').html('Sending E-Mail...');
         event.preventDefault();
 
@@ -85,8 +95,7 @@ $(function(){
                 if (html.success == '1') {
                     $('#button-send').html('Send E-Mail');
                     $('#success').show();
-                }
-                else {
+                } else {
                     $('#button-send').html('Send E-Mail');
                     $('#error').show();
                 }
@@ -96,5 +105,5 @@ $(function(){
                 $('#error').show();
             }
         });
-    }); 
+    });
 });
